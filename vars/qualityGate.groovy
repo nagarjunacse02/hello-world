32c738d9dafbcb37f9e2590f23826ef44a7cc7f7
nagarjunacse02 <32007728+nagarjunacse02@users.noreply.github.com>
2023-12-18 15:01:58 +0530

    Update qualityGate.groovy

--- vars/qualityGate.groovy
+++ vars/qualityGate.groovy
@@ -1,3 +1,10 @@
 def call(credentialsId) {
-    waitForQualityGate abortPipeline: false, credentialsId: credentialsId   
+    waitForQualityGate abortPipeline: false, credentialsId: credentialsId
+    timeout(time: 1, unit: 'HOURS') { 
+    def qg =waitForQualityGate()
+    if(qg.status!= 'OK') {
+      error "Pipeline aborted due to quality gate failure: ${qg.status}"  
+    }
+    }
+    
 }

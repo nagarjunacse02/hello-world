05d258e188263b217592f42f80f92d609bc1f886
nagarjunacse02 <32007728+nagarjunacse02@users.noreply.github.com>
2023-12-21 14:37:05 +0530

    Update artifactsUpload.groovy

--- vars/artifactsUpload.groovy
+++ vars/artifactsUpload.groovy
@@ -1,5 +1,5 @@
 def call(String dockerHubUsername, String imageName) { 
-  docker login -u gorantla.nagarjuna@slkgroup.com fisdemo1.jfrog.io -p Admin@123
+  docker login -u 'gorantla.nagarjuna@slkgroup.com' fisdemo1.jfrog.io -p 'Admin@123'
   docker tag ${imageName} fisdemo1.jfrog.io/demo-fis-docker-docker-local/${imageName}:latest
   docker push fisdemo1.jfrog.io/demo-fis-docker-docker-local/${imageName}:latest
 }

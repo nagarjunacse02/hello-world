f766fce34b687dac0c4ea1d11ff42ac665f59098
nagarjunacse02 <32007728+nagarjunacse02@users.noreply.github.com>
2023-12-21 16:59:33 +0530

    Update artifactsUpload.groovy

--- vars/artifactsUpload.groovy
+++ vars/artifactsUpload.groovy
@@ -1,7 +1,8 @@
 def call(String jfrogArtifactory, String imageName) { 
-  //sh "docker login -u gorantla.nagarjuna@slkgroup.com fisdemo1.jfrog.io -p Admin@123"
-  withCredentials([usernamePassword(credentialsId: 'artifact_creds', passwordVariable: 'pw', usernameVariable: 'un')]) {
-    sh "docker login -u $un fisdemo1.jfrog.io -p $pw"
+  
+  //withCredentials([usernamePassword(credentialsId: 'artifact_creds', passwordVariable: 'pw', usernameVariable: 'un')]) {
+    //sh "docker login -u $un fisdemo1.jfrog.io -p $pw"
+    sh "docker login -u gorantla.nagarjuna@slkgroup.com fisdemo1.jfrog.io -p Admin@123"
     sh "docker tag ${imageName} ${jfrogArtifactory}/${imageName}:latest"
     sh "docker push ${jfrogArtifactory}/${imageName}:latest"
   }
